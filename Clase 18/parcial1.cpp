4b1b19222111f006693096519c9371bee138edfc
Angel <43154124+angel-utn@users.noreply.github.com>
2022-08-11 19:33:17 -0300

    Primer parcial - enunciados y resueltos

---
+++ Clase 18/parcial1.cpp
@@ -0,0 +1,70 @@
+///Ejercicio:
+///Autor:DEK
+///Fecha:
+///Comentario:
+
+# include<iostream>
+# include<cstdlib>
+
+using namespace std;
+
+
+int main(){
+    const int DIAS=2;
+    const int VEHICULOS=5;
+    int patente, dia, kmi, kmf, tipo, importe;
+    int i, j, tipo1=0, tipo2=0;
+    float por1, por2;
+    int totalKm=0, km;
+    int recaudacion=0, precio;
+    ///punto d
+    bool primero=true;
+    int kMin=99999, patenteMin;
+
+    for(i=1; i<=DIAS;i++){
+            for(j=1;j<=VEHICULOS;j++){
+                cin>>dia;
+                cin>>patente;
+                cin>>tipo;
+                cin>>kmi;
+                cin>>kmf;
+                km=kmf-kmi;
+                if(tipo==1){
+                    tipo1++;
+                    precio=60;
+                }
+                else{
+                    tipo2++;
+                    precio=90;
+                }
+                totalKm=totalKm+km;
+                ///punto c
+                if(dia==15){
+                    importe=precio*km;
+                    recaudacion+=importe;
+                }
+                ///punto d
+                if(primero==true){
+                    kMin=km;
+                    patenteMin=patente;
+                    primero=false;
+                }
+                if(km<kMin){
+                    patenteMin=patente;
+                    kMin=km;
+                }
+            }
+    }
+    system("cls");
+    cout<<"punto a";
+    por1=(float)tipo1/(tipo1+tipo2)*100;
+    por2=(float)tipo2/(tipo1+tipo2)*100;
+    cout<<por1<<"\t"<<por2<<endl;
+    cout<<"punto b "<<totalKm<<endl;
+    cout<<"punto c "<<recaudacion<<endl;
+    cout<<"punto d "<<patenteMin;
+	cout<<endl;
+	system("pause");
+	return 0;
+}
+
---
+++ Clase 18/parcial1_2.cpp
@@ -0,0 +1,71 @@
+///Ejercicio:
+///Autor:DEK
+///Fecha:
+///Comentario:
+
+# include<iostream>
+# include<cstdlib>
+
+using namespace std;
+
+
+int main(){
+    const int PATENTES=2;
+    const int DIAS=5;
+    int patente, dia, kmi, kmf, tipo, kmDiario;
+	int i, j;
+	///punto a
+	bool noTrabajo;
+	int sinTrabajo=0;
+	///punto b
+	int totalKm20=0;
+	///punto c
+	int totalRecaudacion=0, precioKm;
+	///punto d
+	int kmMax=0, totalKmMes, autoMax;
+	for(i=1;i<=PATENTES;i++){
+        noTrabajo=false;
+        totalKmMes=0;
+        for(j=1;j<=DIAS;j++){
+            cin>>patente;
+            cin>>dia;
+            cin>>tipo;
+            cin>>kmi;
+            cin>>kmf;
+            kmDiario=kmf-kmi;
+            if(kmDiario==0){
+                noTrabajo=true;
+            }
+            ///punto b
+            if(dia==20){
+                totalKm20=totalKm20+kmDiario;
+            }
+            ///punto c
+            if(tipo==1){
+                precioKm=60;
+            }
+            else{
+                precioKm=90;
+            }
+            totalRecaudacion+=kmDiario*precioKm;
+            ///punto d
+            totalKmMes+=kmDiario;
+        }
+        if(noTrabajo==true){
+            sinTrabajo++;
+        }
+        if(totalKmMes>kmMax){
+            kmMax=totalKmMes;
+            autoMax=patente;
+        }
+	}
+	system("pause");
+	system("cls");
+	cout<<"punto a "<<sinTrabajo<<endl;
+    cout<<"punto b "<<totalKm20<<endl;
+	cout<<"punto c "<<totalRecaudacion<<endl;
+    cout<<"punto d "<<autoMax;
+	cout<<endl;
+	system("pause");
+	return 0;
+}
